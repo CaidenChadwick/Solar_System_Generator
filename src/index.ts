52ea4e5c6e25ef34b8c0feca379a8660475addf4
Sam Krallman <samuel.krallman@smail.astate.edu>
2023-05-04 17:23:24 -0500

    stuff

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,8 @@ import {
   removeMember,
   addGroupSystem,
   removeGroupSystem,
+  getUserGroups,
+  createGroup,
 } from './controllers/GroupController';
 
 const app: Express = express();
@@ -57,6 +59,8 @@ app.post('/api/users/email', updateUserEmail);
 app.post('/api/systems', createSolarSystem);
 app.get('/api/systems/:solarSystemId', getSystem);
 app.get('/systems', getUserSystems);
+app.get('/groups', getUserGroups);
+app.post('/group/:groupId', createGroup);
 // app.post('/api/solarsystem/:solarSystemId/planets', updatePlanet);
 
 // app.get('api/groups/:groupId', getGroup);
